1b8b0deab0007f1865fa5c5b87863eaec95d5201
Albert Kao <albertkao3@gmail.com>
2021-07-10 12:25:54 -0400

    Add Mock.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -20,4 +21,5 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
   }
 
+  heroes = HEROES;
 }
