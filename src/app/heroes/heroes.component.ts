ded9a701cfb79d9f1f7b03336b73cee2a8d488f6
Albert Kao <albertkao3@gmail.com>
2021-07-10 12:44:15 -0400

    Update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,19 +7,25 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  /*
+/*
   hero = 'Windstorm';
-  */
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+  */
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  heroes = HEROES;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
